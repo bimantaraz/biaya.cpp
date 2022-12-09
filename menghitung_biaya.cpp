feb1d0dfb3c6bf8dceab229b70fcebd066a598a9
Anugrah Bimantara <90950617+Bima206@users.noreply.github.com>
2022-12-09 09:38:02 +0700

    Add files via upload

---
+++ menghitung_biaya.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Deklarasi variabel
+  double kecepatan, jarak, waktu, biaya_bahan_bakar;
+  int liter;
+  // Input nilai kecepatan dan jarak
+  cout << "Masukkan kecepatan (km/jam): ";
+  cin >> kecepatan;
+
+  cout << "Masukkan jarak (km): ";
+  cin >> jarak;
+
+  // Hitung waktu dan biaya bahan bakar
+  waktu = jarak / kecepatan;
+  biaya_bahan_bakar = jarak / 20 * 6800;
+  liter = jarak / 20;
+
+  // Tampilkan hasil perhitungan
+  cout << "Waktu perjalanan: " << waktu << " jam" << endl;
+  cout << "Bahan bakar: " << liter << " liter" << endl;
+  cout << "Biaya bahan bakar: Rp" << biaya_bahan_bakar << endl;
+
+  return 0;
+}
